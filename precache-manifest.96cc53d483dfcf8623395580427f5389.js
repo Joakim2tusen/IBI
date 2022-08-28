4ff48128e37a9abc2b9aea76a8fc1e136b700ca8
Joakim Kokocha <joakim2tusen@gmail.com>
2022-08-28 19:04:07 +0200

    deploy

---
+++ precache-manifest.96cc53d483dfcf8623395580427f5389.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f17a42b3a27b4bacd1e",
+    "url": "/IBI/css/app.2378461b.css"
+  },
+  {
+    "revision": "1f17a42b3a27b4bacd1e",
+    "url": "/IBI/js/app.bc7ae619.js"
+  },
+  {
+    "revision": "5d0492b1796ac842a32d",
+    "url": "/IBI/js/chunk-vendors.d0164e9d.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/IBI/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/IBI/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/IBI/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/IBI/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/IBI/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "90b97ac0983022de6dbf0f540a05fa0b",
+    "url": "/IBI/img/bigbonsai.90b97ac0.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/IBI/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/IBI/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/IBI/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/IBI/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/IBI/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "763466c09309423a022b4bc89e657b95",
+    "url": "/IBI/index.html"
+  },
+  {
+    "revision": "90b97ac0983022de6dbf0f540a05fa0b",
+    "url": "/IBI/img/theme/bigbonsai.jpg"
+  },
+  {
+    "revision": "74031dd402ecae3919956ac3d644d3e6",
+    "url": "/IBI/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/IBI/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "9b3c5d84772f5aeeba77f6ee31418345",
+    "url": "/IBI/img/brand/darkgreen.png"
+  },
+  {
+    "revision": "0e13cf9e0215cde9a89ac7a4c25f19c5",
+    "url": "/IBI/img/brand/brightgreen.png"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/IBI/img/brand/favicon_old.ico"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/IBI/img/brand/favicon_old.png"
+  },
+  {
+    "revision": "89e2dce5654010ed622c70fa94a2880d",
+    "url": "/IBI/img/brand/logo.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/IBI/img/brand/white_backup.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/IBI/img/brand/blue.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/IBI/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "053ac9de6d05ff7844c9977bea7bbca1",
+    "url": "/IBI/img/brand/white.png"
+  },
+  {
+    "revision": "2d12a0900a1733e9c2d8023f013592e2",
+    "url": "/IBI/img/brand/favicon_transparent_16x16.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/IBI/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/IBI/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/IBI/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/IBI/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/IBI/img/theme/promo-1.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/IBI/img/theme/profile.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/IBI/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/IBI/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/IBI/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/IBI/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "bad9f9ffefef9363dd41bfdc184ca30b",
+    "url": "/IBI/img/theme/threebonsai.jpg"
+  }
+];
